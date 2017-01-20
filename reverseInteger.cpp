bfe3f597212efb9429d32df3d30ee2aedf9ae233
xianling zhang <alchemxz@gmail.com>
2017-01-19 22:57:23 -0500

    Update reverseInteger.cpp

--- reverseInteger.cpp
+++ reverseInteger.cpp
@@ -1,14 +1,21 @@
+/*
+reverse integer
+ex1: x=123. return 321
+ex2: x=-123, return -321
+*/
+
 class Solution
 {
     public:
     int reverse(int x)
     {
-    int a;
-    int b;
-    int temp;
-    
-    temp=a;
-    a=b;
-    b=temp;
+    long long res=0;
+        while(x)
+        {
+            res=res*10+ x%10;
+            x/=10;
+        }
+        
+        return(res<INT_MIN || res>>INT_MAX) ? 0:res;
     }
-}
+};
